c66235e633055d8c35adce9161c43b5e61f87692
Sandesh Bodake <sandeshbodake@Sandeshs-MacBook-Pro.local>
2020-06-20 23:16:58 +0530

    Sandesh | Project front-end setup done

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Header, PageLayout, Routes, SideNav } from '@components';
+import { CssBaseline } from '@medly-components/core';
+import { store } from '@store';
+import { ThemeProvider } from '@styled';
+import { defaultTheme } from '@theme';
+import React from 'react';
+import { hot } from 'react-hot-loader/root';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+export const App: React.SFC = () => (
+    <Provider store={store}>
+        <ThemeProvider theme={defaultTheme}>
+            <>
+                <CssBaseline />
+                <Router>
+                    <PageLayout>
+                        {/* <SideNav />
+                        <Header />
+                        <Routes /> */}
+                    </PageLayout>
+                </Router>
+            </>
+        </ThemeProvider>
+    </Provider>
+);
+
+export default hot(App);
